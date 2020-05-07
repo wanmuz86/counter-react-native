de084b03c2e7f691ebc99103d49e2f42bc294867
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-07 20:17:21 +0800

    add item

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [number,setNumber] = useState(0)
+  // How to create variable/state on react
+  // number = variable name
+  // setNumber = function to change the variable
+  // useState(0) -> Initialize
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>{number}</Text>
+      <Button title="Increment" color="green" onPress={()=> setNumber(number+1)}/>
+      <Button title="Decrement" color="red" onPress={()=> setNumber(number-1)}/>
+      <Button title="Reset"  onPress={()=> setNumber(0)}/>
     </View>
   );
 }
@@ -16,4 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize:32
+  }
+
 });
