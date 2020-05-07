4c4061da78fa6ec16f3512ee634b73eebc981376
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-05-07 20:37:12 +0800

    add item

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
 export default function App() {
   const [number,setNumber] = useState(0)
-  // How to create variable/state on react
-  // number = variable name
-  // setNumber = function to change the variable
-  // useState(0) -> Initialize
+  // How to create a variable/state on react
+  // number=variable name
+  //function to change variable = setNumber
+  // initialize = useState(0)
+
+  const decrement = () => {
+    if (number == 0){
+      alert('Number cannot be smaller than 0')
+    }
+    else {
+       setNumber(number-1)
+      } 
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>{number}</Text>
       <Button title="Increment" color="green" onPress={()=> setNumber(number+1)}/>
-      <Button title="Decrement" color="red" onPress={()=> setNumber(number-1)}/>
-      <Button title="Reset"  onPress={()=> setNumber(0)}/>
+      <Button title="Decrement" color="red" onPress={decrement }/>
+      <Button title="Reset" onPress={()=> setNumber(0)}/>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -25,7 +32,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   heading: {
-    fontSize:32
+    fontSize: 32
   }
-
 });
